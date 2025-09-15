34d7015a91bab6f9f4c3f796ca55240ed86114e8
ZSRoach <zachary@okmaybe.com>
2025-09-15 11:42:39 -0400

    added scoring

--- My project/Assets/Scripts/BallController.cs
+++ My project/Assets/Scripts/BallController.cs
@@ -4,13 +4,16 @@ public class BallController : MonoBehaviour
 {
     Rigidbody2D rb;
     public float speed;
+    public scoreScript score;
     public Vector2 direction;
+    public int brickCount = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb= GetComponent<Rigidbody2D>();
         direction = Vector2.one.normalized; //(1,1)
+        score = GameObject.FindGameObjectWithTag("Logic").GetComponent<scoreScript>();
     }
 
     // Update is called once per frame
@@ -25,6 +28,9 @@ public class BallController : MonoBehaviour
         if (collision.gameObject.CompareTag("Brick")){
             direction.y = -direction.y;
             Destroy(collision.gameObject);
+            brickCount += 1;
+            score.addScore(1);  
+            Debug.Log("Bricks destroyed: "+brickCount);
         }
         if (collision.gameObject.CompareTag("Wall")){
             direction.x = -direction.x;
@@ -34,6 +40,8 @@ public class BallController : MonoBehaviour
         }
         if (collision.gameObject.CompareTag("Void")){
             Debug.Log("Game over!");
+            gameObject.SetActive(false);
+            score.addScore(0);
         }
     }
 }
---
+++ My project/Assets/Scripts/scoreScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using TMPro;
+
+public class scoreScript : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
+    public GameObject ball;
+    int score = 0;
+
+    public void addScore(int input){
+        if (input == 1){
+            score=score+input;
+            scoreText.text = score.ToString()+" Points";
+            if (score==6){ // for testing purpose
+                winText.text = "You Win!";
+                ball.SetActive(false);
+            }
+        }
+        else if (input == 0){
+            winText.text = "Game Over!";
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
